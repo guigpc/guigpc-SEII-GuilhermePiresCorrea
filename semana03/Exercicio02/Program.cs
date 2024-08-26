96ead01fbdaddcb88c7e616ba26802de897be864
guigpc <guilhermepires.gpc@gmail.com>
2024-08-26 16:41:28 -0300

    0

---
+++ semana03/Exercicio02/Program.cs
@@ -0,0 +1,12 @@
+﻿﻿// Program.cs
+
+using System;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Chama o método para exibir os dias da semana
+            DaysOfWeek.PrintDays();
+        }
+    }
