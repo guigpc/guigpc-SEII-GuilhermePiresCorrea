3e2062c72d9a6ca3d6eab68b1ce55306c3aa8780
guigpc <guilhermepires.gpc@gmail.com>
2024-08-27 13:16:39 -0300

    0

---
+++ semana03/Exercicio03/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO; // Necessário para usar a classe File
+
+namespace Semana03.Exercicio03
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string nomeDoArquivo = "arquivo.txt";
+            string texto = "Este é um documento de texto.";
+
+            // Cria e escreve no arquivo
+            File.WriteAllText(nomeDoArquivo, texto);
+
+            // Mensagem de sucesso
+            Console.WriteLine("Arquivo criado com sucesso!");
+            Console.ReadKey(true);
+        }
+    }
+}
